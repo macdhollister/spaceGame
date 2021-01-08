6658889bde5c2af067635d37c455237db7962596
macdhollister <macdhollister@gmail.com>
2021-01-07 23:14:30 -0800

    Adding Planet, Ship, and Player schemas

--- src/schemas.py
+++ src/schemas.py
@@ -3,6 +3,45 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
+# ----- PLANET -----
+
+class Planet(BaseModel):
+    id: int
+    name: str
+    connections: List['Planet'] = []
+
+
+Planet.update_forward_refs()
+
+
+# ----- SHIP -----
+class Module(BaseModel):
+    pass
+
+
+class Ship(BaseModel):
+    pass
+
+
+# ----- PLAYER -----
+
+class PlayerBase(BaseModel):
+    faction: str
+
+
+class PlayerCreate(PlayerBase):
+    pass
+
+
+class Player(PlayerBase):
+    id: int
+    is_active: bool
+    planets: List[Planet] = []
+    ships: List[Ship] = []
+
+
+# ----- FROM TUTORIAL -----
+
 class ItemBase(BaseModel):
     title: str
     description: Optional[str] = None
