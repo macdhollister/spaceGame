a66a3a997d0ce0b339dbebe49fa9ff61090b718f
macdhollister <macdhollister@gmail.com>
2021-01-16 22:40:16 -0800

    Adding test to create game map

---
+++ tests/integration_test.py
@@ -0,0 +1,74 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src import crud
+from src.models.Base import Base
+
+# In memory database
+DATABASE_URL = "sqlite://"
+
+
+# -------------------- UTILITIES --------------------
+
+@pytest.fixture
+def db():
+    engine = create_engine(
+        DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+    local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    Base.metadata.create_all(bind=engine)
+    db = local_session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+    local_session().close()
+
+
+# -------------------- TESTS --------------------
+
+def test_build_map(db):
+    planets = [
+        {
+            'name': 'planet_a',
+            'size': 's',
+            'resources': 4,
+            'owner': None,
+            'facilities': [],
+            'connections': ['planet_b', 'planet_c']
+        },
+        {
+            'name': 'planet_b',
+            'size': 'm',
+            'resources': 3,
+            'owner': None,
+            'facilities': [],
+            'connections': ['planet_a', 'planet_c']
+        },
+        {
+            'name': 'planet_c',
+            'size': 'l',
+            'resources': 2,
+            'owner': None,
+            'facilities': [],
+            'connections': ['planet_a', 'planet_b']
+        }
+    ]
+
+    game_map = crud.build_map(db, planets)
+
+    assert game_map[0].name == 'planet_a'
+    assert game_map[1].name == 'planet_b'
+    assert game_map[2].name == 'planet_c'
+
+    assert game_map[1] in game_map[0].connections
+    assert game_map[2] in game_map[0].connections
+
+    assert game_map[0] in game_map[1].connections
+    assert game_map[2] in game_map[1].connections
+
+    assert game_map[0] in game_map[2].connections
+    assert game_map[1] in game_map[2].connections
