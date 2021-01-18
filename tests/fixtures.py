06dab8de1854eea746fba4fa6e8da8172614b7c2
macdhollister <macdhollister@gmail.com>
2021-01-17 23:11:06 -0800

    Adding test fixture for map with a faction

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -24,3 +24,30 @@ planets_without_factions = [
             'connections': ['planet_a', 'planet_b']
         }
     ]
+
+planets_with_single_faction = [
+        {
+            'name': 'planet_a',
+            'size': 's',
+            'resources': 4,
+            'owner': 'faction_1',
+            'facilities': [],
+            'connections': ['planet_b', 'planet_c']
+        },
+        {
+            'name': 'planet_b',
+            'size': 'm',
+            'resources': 3,
+            'owner': None,
+            'facilities': [],
+            'connections': ['planet_a', 'planet_c']
+        },
+        {
+            'name': 'planet_c',
+            'size': 'l',
+            'resources': 2,
+            'owner': None,
+            'facilities': [],
+            'connections': ['planet_a', 'planet_b']
+        }
+    ]
