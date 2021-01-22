016a81d4b4225e50c4b6f604d6ce2d183f882772
macdhollister <macdhollister@gmail.com>
2021-01-21 23:43:17 -0800

    Adding method to get current turn

--- src/crud/turn.py
+++ src/crud/turn.py
@@ -1,12 +1,20 @@
+from math import floor
 from sqlalchemy.orm import Session
-from sqlalchemy import asc
 
 from src import models as models
 from src import schemas
+from src.crud.faction import get_factions
 
 
 def get_all_turns(db: Session):
     return db.query(models.Turn).order_by(models.Turn.turn_number.asc()).all()
+
+
+def get_current_turn(db: Session):
+    num_factions = len(get_factions(db))
+    num_turns = len(get_all_turns(db))
+
+    return floor(num_turns/num_factions) + 1
 
 
 def submit_turn(db: Session, turn: schemas.Turn):
