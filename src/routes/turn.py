2094d7ecfcc06f123d24bab976d6612e4f12af5f
macdhollister <macdhollister@gmail.com>
2021-01-22 00:16:09 -0800

    Adding validation on turn submission to check turn number

--- src/routes/turn.py
+++ src/routes/turn.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, HTTPException, Depends
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
+from src import schemas
+from src.crud.faction import get_faction_by_name
+from src.crud.turn import submit_turn
+from src.crud.turn import get_current_turn
 from .utils import get_db
-from src.crud.faction import get_faction_by_name, submit_turn
-from .. import schemas
 
 router = APIRouter(
     prefix="/turns"
@@ -12,16 +15,31 @@ router = APIRouter(
 
 @router.post("/")
 def post_turn(turn: schemas.Turn, db: Session = Depends(get_db)):
+    # TODO faction and turn number should be determined by logged-in user and shouldn't be in the request
     db_owner = get_faction_by_name(db, faction=turn.faction)
     if db_owner is None:
         raise HTTPException(status_code=404, detail="Faction not found")
-    return submit_turn(db, turn)
+
+    # Get state of game
     # TODO
-    #   get current state of game
-    #   validate turn
-    #   validation fails:
-    #       kick back to player
-    #   validation succeeds:
-    #       write to db
-    #       send success message
-    pass
+
+    # Validate orders based on state
+    errors = validate_turn(db, None, turn)
+
+    if errors:
+        return JSONResponse(content={
+            "errors": errors
+        })
+
+    # Save orders to db
+    return submit_turn(db, turn)
+
+
+def validate_turn(db, game_state, turn):
+    errors = []
+
+    current_turn = get_current_turn(db)
+    if turn.turn_number != current_turn:
+        errors.append("You may not submit a turn until all players have submitted.")
+
+    return errors
